ff274f8f8b0524e050e964a22b77679b0af99828
Gülay ADIGÜZEL <glyadgzl@hotmail.com>
2025-05-22 15:40:52 +0300

    Add FirebaseController to manage data retrieval from Firebase Database

---
+++ cv/lib/controller/firebase_controller.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_database/firebase_database.dart';
+
+class FirebaseController {
+  final DatabaseReference _aboutRef =
+      FirebaseDatabase.instance.ref().child("Portfolio/about_us");
+  final DatabaseReference _experienceRef =
+      FirebaseDatabase.instance.ref().child("Portfolio/experience");
+  final DatabaseReference _portfolioRef =
+      FirebaseDatabase.instance.ref().child("Portfolio/portfolio");
+
+  Future<String> getFullName() async {
+    final snapshot = await _aboutRef.get();
+    final data = snapshot.value as Map?;
+    return data?['name'] ?? 'No name found';
+  }
+
+  Future<String> getDesignation() async {
+    final snapshot = await _aboutRef.get();
+    final data = snapshot.value as Map?;
+    return data?['designation'] ?? 'No designation found';
+  }
+
+  Future<String> getDescription() async {
+    final snapshot = await _aboutRef.get();
+    final data = snapshot.value as Map?;
+    return data?['description'] ?? 'No description found';
+  }
+}
