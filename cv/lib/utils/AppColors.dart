555fa9f329c844e5748ec8170ead40f5923c36a2
Gülay ADIGÜZEL <glyadgzl@hotmail.com>
2025-05-26 14:54:03 +0300

    Update navigation selection color for consistency in theme

--- cv/lib/utils/AppColors.dart
+++ cv/lib/utils/AppColors.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 
 Color backgroundLight = Color(0xffffffff);
 Color iconTint = Color(0xff888787);
-Color navigationSelectionColor = Color(0xffffc107);
+Color navigationSelectionColor = Color(0xFF5B86E5);
 Color cardBGColor = Color(0xfffbfbfb);
