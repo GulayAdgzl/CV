d36525ac50ade7ddb01a29ab96aa1b1ded021d06
Gülay ADIGÜZEL <glyadgzl@hotmail.com>
2025-06-30 16:51:37 +0300

    feat: restructure Firebase initialization and migrate HomePage to a new location; remove old HomePage implementation

---
+++ cv/lib/firebase/firebase_init.dart
@@ -0,0 +1,8 @@
+import 'package:cv/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+Future<void> initializeFirebase() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+}
--- cv/lib/main.dart
+++ cv/lib/main.dart
@@ -1,19 +1,16 @@
-import 'package:cv/pages/home_page.dart';
+import 'package:cv/firebase/firebase_init.dart';
+import 'package:cv/pages/views/home_page.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart'; // Bu dosya flutterfire configure ile oluşur
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
-  runApp(MyApp());
+  await initializeFirebase();
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
