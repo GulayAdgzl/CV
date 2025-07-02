e88b672d03fbb17a5cee464a2582ebf5eca27592
Gülay ADIGÜZEL <glyadgzl@hotmail.com>
2025-07-02 22:03:45 +0300

    feat: refactor theme configuration; streamline imports and update ThemeData structure

--- cv/lib/core/theme.dart
+++ cv/lib/core/theme.dart
@@ -2,20 +2,16 @@
 import 'package:cv/utils/AppColors.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-// lib/theme/app_theme.dart
 
-import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'gradient_theme_extension.dart';
 
 final ThemeData appTheme = ThemeData(
   useMaterial3: true,
+  scaffoldBackgroundColor: AppColors.backgroundLight,
   colorScheme: ColorScheme.fromSeed(
     seedColor: AppColors.navigationSelectionColor,
     background: AppColors.backgroundLight,
   ),
-  scaffoldBackgroundColor: AppColors.backgroundLight,
-  cardColor: AppColors.cardBGColor,
-  iconTheme: const IconThemeData(color: AppColors.iconTint),
   textTheme: GoogleFonts.poppinsTextTheme().copyWith(
     headlineSmall: const TextStyle(
       fontSize: 22,
@@ -27,4 +23,13 @@ final ThemeData appTheme = ThemeData(
       color: Colors.black87,
     ),
   ),
+  extensions: const <ThemeExtension<dynamic>>[
+    GradientTheme(
+      backgroundGradient: LinearGradient(
+        colors: [Color(0xFFE3F2FD), Color(0xFFFFFFFF)],
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+      ),
+    ),
+  ],
 );
